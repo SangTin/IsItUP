3530d97c782a79e6faa64e4102e49e0ccf244a76
SangTin <sang080304@gmail.com>
2024-06-08 16:22:13 +0700

    add flag to check complete

--- feature/Verifier.js
+++ feature/Verifier.js
@@ -7,13 +7,13 @@ let Regex = require('../util/Regex.js');
 
 class Verifier {
     static verifyUrl(msg, match, callback) {
-        //  setup according url
+        // setup according url
         let request = http;
         let url = match[0];
 
         if (url.match(Regex.justVerifyRegex)) return;
 
-        //  verify if its https or doesn`t have protocol
+        // verify if its https or doesn`t have protocol
         if (match[2] == "s") {
             request = https;
         } else if (match[1] == undefined) {
@@ -22,20 +22,30 @@ class Verifier {
 
         console.log(`Got: ${url}`);
 
+        let requestCompleted = false;
+
         const req = request.get(url, res => {
-            console.log(`URL: ${url} || Status Code: ${res.statusCode}`);
-            callback(msg, url, true, res.statusCode);
+            if (!requestCompleted) {
+                requestCompleted = true;
+                console.log(`URL: ${url} || Status Code: ${res.statusCode}`);
+                callback(msg, url, true, res.statusCode);
+            }
         }).on('error', e => {
-            // is not responding
-            console.log(`URL error: ${url}`);
-            callback(msg, url, false);
+            if (!requestCompleted) {
+                requestCompleted = true;
+                console.log(`URL error: ${url}`);
+                callback(msg, url, false);
+            }
         });
 
         // Set timeout for the request
         req.setTimeout(10000, () => { // 10 seconds
-            console.log(`URL timeout: ${url}`);
-            req.abort();
-            callback(msg, url, false);
+            if (!requestCompleted) {
+                requestCompleted = true;
+                console.log(`URL timeout: ${url}`);
+                req.abort();
+                callback(msg, url, false);
+            }
         });
     }
 }
